9c72c772b863d3bf06b70db045fcbf14524a6c0a
entropyQueen <emy.rg@protonmail.com>
2023-06-18 13:50:27 +0200

    change version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='readit',
-    version='1.0',
+    version='0.0.1',
     description='readit - A command line tool to read a text file aloud',
     author='Emy Canton',
     author_email='emy.canton@proton.me',
